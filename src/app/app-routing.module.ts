956730206e54afa1479da2ccbe6643eb0d82eff1
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2022-01-22 18:17:30 -0500

    removed home page placeholder code and replaced it with router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CategoryComponent } from './components/category/category/category.component';
+
+const routes: Routes = [
+  { path: '', component: CategoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
