e1c543086163dd266b259e43d292cb9caee6d186
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2022-01-22 17:21:49 -0500

    added category service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CategoryService } from './services/categoryService/category.service';
 
 @NgModule({
+  bootstrap: [
+    AppComponent
+  ],
   declarations: [
     AppComponent
   ],
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    CategoryService
+  ]
 })
+
 export class AppModule { }
