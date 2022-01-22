8404045e5d3bb0a4761349b3fa4de92b898b8851
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2022-01-22 17:34:15 -0500

    added category component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CategoryComponent } from './components/category/category/category.component';
 import { CategoryService } from './services/categoryService/category.service';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { CategoryService } from './services/categoryService/category.service';
     AppComponent
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
