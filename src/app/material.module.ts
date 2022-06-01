64a92a2d5443cdceec3df0058d66ef8879e80024
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2022-06-01 12:08:16 -0400

    Added tabs to category options modal

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,15 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     exports: [
         MatButtonModule,
         MatDialogModule,
-        MatTableModule
+        MatTableModule,
+        MatTabsModule
     ]
 })
 
